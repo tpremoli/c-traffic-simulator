9ff95931aaf8aa102d9cdba6795319ee25320da5
Tomas Premoli Muniagurria <tmp211@emps-ugcs1.ex.ac.uk>
2022-03-23 21:52:33 +0000

    Began implementing runOneSimulation

--- cw/runSimulations.c
+++ cw/runSimulations.c
@@ -1,8 +1,43 @@
-# include <stdio.h>
-# include <gsl/gsl_rng.h>
-# include <gsl/gsl_randist.h>
+#include <stdio.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
 
-int main(){
+int runOneSimulation(float leftFlow, float rightFlow, int leftTime, int rightTime);
+
+
+int runOneSimulation(float leftFlow, float rightFlow, int leftTime, int rightTime)
+{
+    unsigned int leftQueue = 0;
+    unsigned int rightQueue = 0;
+
+    const gsl_rng_type *T;
+    gsl_rng *r;
+    gsl_rng_env_setup();
+    T = gsl_rng_default;
+    r = gsl_rng_alloc(T);
+
+    gsl_rng_set(r, time(0));
+
+    char rightOn;
+    char leftOn;
+
+    /* Determining which light is on to begin with */
+    if (gsl_ran_flat(r, 0, 1) < 0.5)
+    {
+        leftOn = 0;
+        rightOn = 1;
+    }
+    else
+    {
+        leftOn = 1;
+        rightOn = 0;
+    }
+
+    return 0;
+}
+
+int main()
+{
 
     printf("hello.txt");
 
