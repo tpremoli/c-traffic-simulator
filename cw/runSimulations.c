7708355949bb01c0ceb1714f3fe446081e5d71a4
Tomas Premoli Muniagurria <tmp211@emps-ugcs1.ex.ac.uk>
2022-03-25 13:37:52 +0000

    Fixes and comments

--- cw/runSimulations.c
+++ cw/runSimulations.c
@@ -6,6 +6,7 @@ int runOneSimulation(float leftFlow, float rightFlow, int leftTime, int rightTim
 
 int runOneSimulation(float leftFlow, float rightFlow, int leftTime, int rightTime)
 {
+    /* RNG setup */
     const gsl_rng_type *T;
     gsl_rng *r;
     gsl_rng_env_setup();
@@ -25,7 +26,6 @@ int runOneSimulation(float leftFlow, float rightFlow, int leftTime, int rightTim
     unsigned int timeTillLeftRed;
     unsigned int timeTillRightRed;
     unsigned int remainingIterations = 500;
-
 
     /* Determining which light is on to begin with */
     if (gsl_ran_flat(r, 0, 1) < 0.5)
@@ -57,46 +57,54 @@ int runOneSimulation(float leftFlow, float rightFlow, int leftTime, int rightTim
 
     while (remainingIterations != 0 || (leftQueue != 0 || rightQueue != 0))
     {
-        if(timeTillLeftRed == 0){
+        /* Possibility 1: Lights flipping */
+        if (timeTillLeftRed == 0)
+        {
             rightGreen = 1;
             leftGreen = 0;
             timeTillRightRed = rightTime;
             timeTillLeftRed--;
-        }else if(timeTillRightRed == 0){
+        }
+        else if (timeTillRightRed == 0)
+        {
             rightGreen = 0;
             leftGreen = 1;
             timeTillLeftRed = leftTime;
             timeTillRightRed--;
-        }else{
-
+        }
+        /* Possibility 2: Cars move */
+        else
+        {
             /* Step 1: Queue on left */
             if (gsl_ran_flat(r, 0, 1) < leftFlow && (remainingIterations != 0))
             {
                 leftQueue = leftQueue + 1;
                 totalLeftCars = totalLeftCars + 1;
             }
+
             /* Step 2: Queue on right */
             if (gsl_ran_flat(r, 0, 1) < rightFlow && (remainingIterations != 0))
             {
                 rightQueue = rightQueue + 1;
                 totalRightCars = totalRightCars + 1;
             }
-            /* Step 3: Cars move */
+
+            /* Step 3: Cars moving */
             if (leftGreen == 1 && leftQueue != 0)
             {
                 leftQueue = leftQueue - 1;
             }
-            if (rightGreen == 1 && rightQueue != 0)
+            else if (rightGreen == 1 && rightQueue != 0)
             {
                 rightQueue = rightQueue - 1;
             }
 
             timeTillRightRed--;
             timeTillLeftRed--;
-
         }
 
-        if(remainingIterations != 0)
+        /* Step 4: Are cars done arriving? */
+        if (remainingIterations != 0)
             remainingIterations--;
 
         printf("remainingiterations: %d. rightGreen = %d. leftGreen = %d. leftQueue = %d, rightQueue = %d\n", remainingIterations, rightGreen, leftGreen, leftQueue, rightQueue);
